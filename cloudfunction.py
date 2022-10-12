09caaf92204adb6628cf6bb7f0510129370f7d11
Clickba <ertandre@gmail.com>
2022-10-12 15:42:30 +0100

    zzzzzz

---
+++ cloudfunction.py
@@ -0,0 +1,10 @@
+from google.cloud import pubsub_v1
+from google.cloud import storage
+def hello_gcs(event, context):
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = "projectsgcp-6th-academy-364009topicstopic_demo_acasa"
+    data_str = "Message aaaaaaaaaaaaaaaaaaaaaaa"
+    # Data must be a bytestring
+    data = data_str.encode(utf-8)
+    # When you publish a message, the client returns a future.
+    publisher.publish(topic_path, data)
